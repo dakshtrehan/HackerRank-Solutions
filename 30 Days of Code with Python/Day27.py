c8a616272dd5ed6b6699028e455029ee6ecbc7ef
Daksh Trehan <daksh.trehan@hotmail.com>
2021-06-01 10:09:34 +0530

    Add files via upload

---
+++ 30 Days of Code with Python/Day27.py
@@ -0,0 +1,35 @@
+
+class TestDataEmptyArray(object):
+    
+    @staticmethod
+    def get_array():
+        # complete this function
+        return []
+
+class TestDataUniqueValues(object):
+
+    @staticmethod
+    def get_array():
+        # complete this function
+        return [1,2]
+
+    @staticmethod
+    def get_expected_result():
+        # complete this function
+        return 0
+
+class TestDataExactlyTwoDifferentMinimums(object):
+
+    @staticmethod
+    def get_array():
+        # complete this function
+        return [0,0]
+
+    @staticmethod
+    def get_expected_result():
+        # complete this function
+        return 0
+        
+        
+        
+
