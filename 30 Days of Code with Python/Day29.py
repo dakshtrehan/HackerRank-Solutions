59759453581faa77a04734eb9a602b941ecc97c4
Daksh Trehan <daksh.trehan@hotmail.com>
2021-06-03 10:26:37 +0530

    Add files via upload

---
+++ 30 Days of Code with Python/Day29.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bitwiseAnd' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
+#
+
+def bitwiseAnd(N, K):
+    # Write your code here
+    mx_b = 0
+    for x in range(1,N+1):
+        for y in range(1,x):
+            b = x&y
+            if mx_b<b<K:
+                mx_b = b
+                if mx_b == K-1:
+                    return mx_b
+    return mx_b        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
